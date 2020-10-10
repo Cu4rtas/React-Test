5b38832feda2794a8c5dddbc31cf14083bb604f0
JQartas <jcuartas0611@gmail.com>
2020-10-10 16:23:08 -0500

    Updates

--- src/state/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import app from './app';
-import todos from './todos';
-import todoslog from './log';
-
-export default combineReducers({ app, todos, todoslog });
