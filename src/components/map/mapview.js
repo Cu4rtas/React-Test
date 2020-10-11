1c178c15f105655e7e807ce4abbdea91dce2f841
JQartas <jcuartas0611@gmail.com>
2020-10-11 12:02:21 -0500

    movil map view improvement

--- src/components/map/mapview.js
+++ src/components/map/mapview.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import './mapview.css';
 import GoogleMapReact from 'google-map-react';
 import MapMarker from './marker';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { v1 as uuid } from 'uuid';
-import './mapview.css';
 
 const MapView = () => {
 	const position = useSelector((state) => state.app.position);
@@ -17,26 +17,29 @@ const MapView = () => {
 		zoom: 15
 	};
 	return (
-		<Container className="mt-4 shadow p-2 overflow-auto m-view">
-			<GoogleMapReact
-				bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
-				center={defaultSettings.center}
-				defaultZoom={defaultSettings.zoom}
-				yesIWantToUseGoogleMapApiInternals
-			>
-				{todoslog.map((log) => {
-					return (
-						<MapMarker
-							lat={log.position.lat}
-							lng={log.position.lng}
-							action={log.action}
-							name={log.name}
-							from={log.from || false}
-							key={uuid()}
-						/>
-					);
-				})}
-			</GoogleMapReact>
+		<Container className="mt-4 shadow p-2 overflow-auto">
+			<i className='text-yellow-700 ml-3 mb-1 block'>Press on the marker to see more...</i>
+			<Container className="m-view">
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
+					center={defaultSettings.center}
+					defaultZoom={defaultSettings.zoom}
+					yesIWantToUseGoogleMapApiInternals
+				>
+					{todoslog.map((log) => {
+						return (
+							<MapMarker
+								action={log.action}
+								from={log.from || false}
+								key={uuid()}
+								lat={log.position.lat}
+								lng={log.position.lng}
+								name={log.name}
+							/>
+						);
+					})}
+				</GoogleMapReact>
+			</Container>
 		</Container>
 	);
 };
